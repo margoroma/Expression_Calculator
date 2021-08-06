601f252a8afd1e9b0146f6f489d666742abea968
margoroma <margoroma2010@gmail.com>
2021-08-06 10:49:48 +0300

    feat: added app for calculator

---
+++ app_Calc.py
@@ -0,0 +1,8 @@
+from Calculator_with_brackets import recurs
+
+
+"""Enter the expression or continue with default expression"""
+expression = '(25 -(5- (1-2))/(5-8))'
+# equation = input('Expression is: \n')
+results = float(recurs(expression))
+print(f'Result is: {results}')
